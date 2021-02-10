2af29ea69ccb49c8af0d068049fe37dee841d133
rochards <rodrigorochasantos@outlook.com>
2021-02-10 16:17:49 -0300

    Criadas instancias iniciais para popular o H2 no inicio da aplicacao.

--- src/main/java/com/rochards/sistemavendas/SistemaVendasApplication.java
+++ src/main/java/com/rochards/sistemavendas/SistemaVendasApplication.java
@@ -1,13 +1,29 @@
 package com.rochards.sistemavendas;
 
+import com.rochards.sistemavendas.domain.Categoria;
+import com.rochards.sistemavendas.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SistemaVendasApplication {
+public class SistemaVendasApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaVendasApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
